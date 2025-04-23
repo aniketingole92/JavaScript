6c228ff40080187aead48caa3e5482e3796c09de
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-04-23 16:41:11 +0530

    Create loop.js

---
+++ loop.js
@@ -0,0 +1,17 @@
+// for loop
+for(let i = 1; i <= 5; i++) {
+    console.log(i);  // 1, 2, 3, 4, 5
+}
+
+// while loop
+let count = 1;
+while(count <= 3) {
+    console.log(count);  // 1, 2, 3
+    count++;
+}
+
+// for-of loop (arrays ke liye)
+let fruits = ["Apple", "Banana", "Mango"];
+for(let fruit of fruits) {
+    console.log(fruit);
+}
