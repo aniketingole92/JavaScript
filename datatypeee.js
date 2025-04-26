5d59837b2652ef10b5c3aa019c437375eee15a96
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-04-26 08:52:05 +0530

    Create datatypeee.js

---
+++ datatypeee.js
@@ -0,0 +1,8 @@
+typeof "John"      // "string"
+typeof 3.14        // "number"
+typeof true        // "boolean"
+typeof undefined   // "undefined"
+typeof null        // "object" (historical bug)
+typeof {a: 1}      // "object"
+typeof [1, 2, 3]   // "object"
+typeof function(){} // "function"
