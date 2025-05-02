7479b5f155969b10d37f9b6261442868ff5bb0e6
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-05-02 16:06:36 +0530

    Create nightin.js

---
+++ nightin.js
@@ -0,0 +1,9 @@
+function sign(){
+	document.getElementById("signinText");
+}
+console.log("Aniket Ingole");
+let aniket = 2.3;
+console.log(typeof(aniket));
+let yed = "Amit";
+console.log(typeof(yed));
+
