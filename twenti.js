895746eea6e81b88ddee403be6b46d2918edf7e2
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-05-02 16:07:20 +0530

    Create twenti.js

---
+++ twenti.js
@@ -0,0 +1,14 @@
+let company = true;
+console.log(typeof(company));
+
+let agency = 4.5;
+console.log(typeof(agency));
+
+let assig ;
+console.log(typeof(assig));
+
+let sddd = "Aniket";
+console.log(typeof(sddd));
+
+
+
