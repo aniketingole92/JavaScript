4d9a89b109ed3b10f9362d932714c91422d73217
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-05-04 15:28:03 +0530

    Create thirtin.js

---
+++ thirtin.js
@@ -0,0 +1,4 @@
+let h1Element = document.CreateElement('h1');
+h1Element.textContent = "Web Technologics";
+let ContainerElement = document.getElementById('mycontainer');
+ContainerElement.appendChild(h1Element);
