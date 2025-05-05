6eafd26b85657870fbcc94701482eb24c520b572
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-05-05 15:32:22 +0530

    Create thirthree.js

---
+++ thirthree.js
@@ -0,0 +1,20 @@
+let person = {
+	name : "Rahul",
+	age : 28,
+	income : 345,
+	run : function(){
+		console.log("Starting Running")
+	}
+};
+person.run();
+
+let person1 = {
+	name : "Aniket",
+	age : 20,
+	income : 10,
+	par : function(){
+		console.log("Aniket Best Frineds is Parmeshwar Sawant")
+	}
+};
+person1.par();
+
