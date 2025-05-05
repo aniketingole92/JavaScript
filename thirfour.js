4b741449f863915a8db466870bf293d79197f19d
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-05-05 15:45:29 +0530

    Create thirfour.js

---
+++ thirfour.js
@@ -0,0 +1,15 @@
+let person = {
+	name : "Aniket",
+	age : 20,
+	habit: ["Dance" , "Swiming"],
+	car:{
+		name:"Audi",
+		model:"A6",
+		color:"white"
+	}
+	
+};
+console.log(person.car.name);
+console.log(person.car['model']);
+console.log(person.habit);
+
