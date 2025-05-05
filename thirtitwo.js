04c621b76487cbeebf2245d9ca6d9638528583f7
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-05-05 15:31:52 +0530

    Create thirtitwo.js

---
+++ thirtitwo.js
@@ -0,0 +1,32 @@
+let person = {
+	name:"Aniket",
+	age : 20
+};
+person.name = "Angad";
+console.log(person.name);
+
+let person1 = {
+	name:"Rahul",
+	age : 28
+};
+person1['name'] = "Abhi";
+console.log(person1['name']);
+
+let person2 = {
+	name : "Abhinav",
+	age : 21,
+	clases : "BCA" ,
+	income : 20
+};
+person2.name = "Aniket";
+console.log(person2);
+ 
+let person3 = {
+	name: "Anikettt",
+	age : 20,
+	income: 30
+};
+person3.name = "Aniket";
+person3.gender = "Male";
+console.log(person3);
+
