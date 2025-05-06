a6cd472648f87b49cd2c422cdb39c38bfe9bceb3
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-05-06 15:35:19 +0530

    Create todo-container.js

---
+++ todo-container.js
@@ -0,0 +1,4 @@
+let todoItemsContainer = document.getElementById("todoItemsContainer");
+let todoElement = document.createElement("li");
+todoElement.classList.add("todo-item-container", "d-flex" , "flex-row");
+todoItemsContainer.appendChild(todoElement);
