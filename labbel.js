a2b190827ed3e054b3b7c7b65a03b7998f255742
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-05-07 16:45:49 +0530

    Create labbel.js

---
+++ labbel.js
@@ -0,0 +1,6 @@
+
+let labelElement = document.createElement("label");
+labelElement.setAttribute("for", "checkbox-input");
+labelElement.classList.add("checkbox-label");
+labelElement.textContent = "Learn HTML";
+labelcontainer.appendChild(labelElement);
