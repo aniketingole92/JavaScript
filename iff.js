abd4270d5b6edcc98053930f66794344fc7d5af5
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-05-08 15:25:38 +0530

    Create iff.js

---
+++ iff.js
@@ -0,0 +1,6 @@
+let todoList = getTodoListFromLocalStorage();
+let todosCount = todoList.length;
+
+saveTodoButton.onclick = function () {
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+};
