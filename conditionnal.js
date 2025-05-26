f7df835f89d5e06239757f150eefc03093490266
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-05-26 19:05:52 +0530

    Create conditionnal.js

---
+++ conditionnal.js
@@ -0,0 +1,16 @@
+// If-else
+let score = 85;
+if (score >= 90) {
+  console.log("Grade A");
+} else if (score >= 80) {
+  console.log("Grade B");
+} else {
+  console.log("Grade C");
+}
+// Output: Grade B
+
+// For loop
+for (let i = 0; i < 3; i++) {
+  console.log("Iteration " + i);
+}
+// Output: Iteration 0, Iteration 1, Iteration 2
