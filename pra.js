4859561c1221b3c1ac09543253ddeef98424c38b
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-05-29 18:52:10 +0530

    Create pra.js

---
+++ pra.js
@@ -0,0 +1,17 @@
+let str = "Hello";
+let num = 123;
+let bool = true;
+let n = null; // Intentionally no value
+let und;      // Value not yet assigned
+let sym = Symbol("id");
+let bigNum = 1234567890123456789012345678901234567890n;
+
+let obj = { name: "Alice" };
+let arr = [1, 2, 3];
+let func = function() { console.log("Hi"); };
+
+console.log(typeof str);   
+console.log(typeof num);   
+console.log(typeof bool);   
+console.log(typeof n);      
+console.log(typeof und);    
