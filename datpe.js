99516a6dae39decc3ef66d6c32e5609f0eb2b918
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-06-02 15:12:03 +0530

    Create datpe.js

---
+++ datpe.js
@@ -0,0 +1,16 @@
+let x = 10, y = 5;
+
+// Arithmetic operators
+console.log(x + y); // 15 (Addition)
+console.log(x - y); // 5 (Subtraction)
+console.log(x * y); // 50 (Multiplication)
+console.log(x / y); // 2 (Division)
+
+// Comparison operators
+console.log(x > y);  // true
+console.log(x === y); // false (strict equality)
+
+// Logical operators
+console.log(true && false); // AND (false)
+console.log(true || false); // OR (true)
+console.log(!true);         // NOT (false)
