543edd23611f7bc06f2026b25a39a21fc382d2a8
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-06-04 10:38:29 +0530

    Create fundam.js

---
+++ fundam.js
@@ -0,0 +1,15 @@
+let str = "Hello, World!"; // String
+let num = 100;            // Number
+let isActive = false;     // Boolean
+let notDefined;           // Undefined
+let empty = null;         // Null
+let obj = { name: "Rahul", age: 25 }; // Object
+let arr = [1, 2, 3, "four"];          // Array
+
+console.log(typeof str); // Output: string
+console.log(typeof num); // Output: number
+console.log(typeof isActive); // Output: boolean
+console.log(typeof notDefined); // Output: undefined
+console.log(typeof empty); // Output: object
+console.log(obj); // Output: { name: 'Rahul', age: 25 }
+console.log(arr); // Output: [1, 2, 3, 'four']
